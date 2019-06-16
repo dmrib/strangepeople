b4f5219515e5dda401ed542d57f5f5dd462537bd
dmrib <dmrib.cs@gmail.com>
2019-06-16 09:54:27 -0300

    Implement initial population generation

--- strangepeople/ga/sketch.js
+++ strangepeople/ga/sketch.js
@@ -1,5 +1,30 @@
+const POOL = '!@#$%^&*()_+~`|}{[]\:;?><,./-=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const TARGET = 'Strange people doing strange things with software.'
+
+class GeneticAlgorithm {
+	constructor(target, populationSize) {
+		this.populationSize = populationSize;
+		this.population = [];
+		this.createInitialPopulation();
+	}
+
+	createInitialPopulation() {
+		for(let i=0; i<this.populationSize; i++) {
+			let individual = [];
+			for(let j=0; j<TARGET.length; j++) {
+				individual.push(POOL.charAt(random(0, POOL.length)));
+			}
+			this.population.push(individual);
+		}
+	}
+}
+
+let ga;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+
+	ga = new GeneticAlgorithm(TARGET, 100);
 }
 
 function draw() {

eab33816d04a0f9c8173b4929a14e0503afe6f33
dmrib <dmrib.cs@gmail.com>
2020-02-16 09:55:01 -0300

    Code documentation improvements

--- src/geneticalgorithm.js
+++ src/geneticalgorithm.js
@@ -1,107 +1,256 @@
-class GeneticAlgorithm {
-	constructor(target, populationSize, mutationRate) {
+/**
+ * Genetic algorithm abstraction.
+ */
+
+class GeneticAlgorithm
+{
+    /**
+     * Constructor.
+     *
+     * Args:
+     *  target(string): evolution target string
+     *  populationSize(number): size of gene pool
+     *  mutationRate(number): mutation rate percentage
+     *
+     * Returns:
+     *  undefined.
+     */
+    constructor(target, populationSize, mutationRate)
+    {
+        // store components
 		this.generation = 0;
 		this.mutationRate = mutationRate;
 		this.populationSize = populationSize;
-		this.population = [];
+        this.population = [];
+
+        // create initial gene pool
 		this.createInitialPopulation();
 	}
 
-	createInitialPopulation() {
-		for(let i=0; i<this.populationSize; i++) {
-			let individual = [];
-			for(let j=0; j<TARGET.length; j++) {
+    /**
+     * I create my initial gene pool.
+     *
+     * Returns:
+     *  undefined.
+     */
+    createInitialPopulation()
+    {
+        // for the entire population size
+        for(let i=0; i<this.populationSize; i++)
+        {
+            // create empty individual genotype
+            let individual = [];
+
+            // create random gene until target size is met
+            for(let j=0; j<TARGET.length; j++)
+            {
 				individual.push(POOL.charAt(random(0, POOL.length)));
-			}
+            }
+
+            // add individual genotype to gene pool
 			this.population.push(individual);
 		}
 	}
 
-	computeFitness() {
-		let populationFitness = [];
-		let bestFitness = -1;
+    /**
+     * I compute the fitness of my gene pool.
+     *
+     * Returns:
+     *  fitness(object): object containing fitness status info
+     */
+    computeFitness()
+    {
+        // create fitness values container
+        let populationFitness = [];
+
+        // setup best fitness found value
+        let bestFitness = -1;
+
+        // setup best fitness index container
 		let bestFitnesssPosition;
 
-		for(const [index, individual] of this.population.entries()) {
-			let fitness = 0;
-			for(let i=0; i<individual.length; i++) {
-				if(individual[i] === TARGET.charAt(i)) {
+        // for each individual in population
+        for(const [index, individual] of this.population.entries())
+        {
+            // start fitness score counter
+            let fitness = 0;
+
+            // for each gene in individual's genotype
+            for(let i=0; i<individual.length; i++)
+            {
+                // gene matches target: increase fitness score counter
+                if(individual[i] === TARGET.charAt(i))
+                {
 					fitness++;
 				}
-			}
+            }
+
+            // add individual fitness to report
 			populationFitness.push(fitness);
 
-			if(bestFitness < fitness) {
+            // new best fitness found: update report
+            if(bestFitness < fitness)
+            {
 				bestFitness = fitness;
 				bestFitnesssPosition = index;
 			}
 		}
 
+        // return fitness report
 		return {populationFitness, bestFitness, bestFitnesssPosition};
 	}
 
-	buildMatingPool(populationFitness) {
+    /**
+     * I build this generation mating pool.
+     *
+     * Args:
+     *  populationFitness(Array): population fitness values
+     *
+     * Returns:
+     *  matingPool(Array): individuals indexes for mating
+     */
+    buildMatingPool(populationFitness)
+    {
+        // initialize mating pool
 		let matingPool = [];
 
-		for(const [index, fitness] of populationFitness.entries()) {
-			for(let i=0; i<fitness; i++) {
+        // for entry in population fitness report
+        for(const [index, fitness] of populationFitness.entries())
+        {
+            // replicate individual in pool based on fitness values
+            for(let i=0; i<fitness; i++)
+            {
 				matingPool.push(index);
 			}
 		}
 
+        // return mating pool
 		return matingPool;
 	}
 
-	mutate() {
-		let roll;
+    /**
+     * I apply mutation in my current population.
+     *
+     * Returns:
+     *  undefined.
+     */
+    mutate()
+    {
+        // for each individual of population
+        for(let individual of this.population)
+        {
+            // for each gene fo individual
+            for(let i=0; i<individual.length; i++)
+            {
+                // get random value between 0 and 1
+                const roll = random();
 
-		for(let individual of this.population) {
-			for(let i=0; i<individual.length; i++) {
-				roll = random();
-				if(roll < this.mutationRate) {
+                // random value is less than mutation rate: mutate individual
+                if(roll < this.mutationRate)
+                {
 					individual[i] = POOL.charAt(random(0, POOL.length));
 				}
 			}
 		}
 	}
 
-	mate(populationFitness) {
+    /**
+     * I mate my current population.
+     *
+     * Args:
+     *  populationFitness: population fitness values
+     *
+     * Returns:
+     *  undefined.
+     */
+    mate(populationFitness)
+    {
+        // build current mating pool
 		const matingPool = this.buildMatingPool(populationFitness);
 
+        // create next population container
 		let nextPopulation = [];
 
-		for(let i=0; i<this.populationSize; i++) {
-			let father = this.population[matingPool[int(random(0, matingPool.length))]];
-			let mother = this.population[matingPool[int(random(0, matingPool.length))]];
+        // until next population is fully created
+        for(let i=0; i<this.populationSize; i++)
+        {
+            // select a father
+            let father = this.population[matingPool[int(random(0, matingPool.length))]];
+
+            // select a mother
+            let mother = this.population[matingPool[int(random(0, matingPool.length))]];
+
+            // create child
 			let child = this.crossover(father, mother);
 
+            // add child to next population
 			nextPopulation.push(child);
 		}
 
+        // update population
 		this.population = nextPopulation;
 	}
 
-	crossover(father, mother) {
-		let child = [];
-		for(let i=0; i<father.length; i++) {
-			let roll = random();
-			if(roll < 0.5) {
+    /**
+     * I crossover two genotypes.
+     *
+     * Args:
+     *  father(Array): father genotype
+     *  mother(Array): mother genotype
+     *
+     * Returns:
+     *  child(Array): combined genotype
+     */
+    crossover(father, mother)
+    {
+        // create child container
+        let child = [];
+
+        // for genotype size
+        for(let i=0; i<father.length; i++)
+        {
+            // get random value between 0 and 1
+            let roll = random();
+
+            // random value is lesser than threshold: get father's gene
+            if(roll < 0.5)
+            {
 				child.push(father[i]);
-			}
-			else {
+            }
+
+            // random value is higher than threshold: get mother's gene
+            else
+            {
 				child.push(mother[i]);
 			}
 		}
 
+        // return child
 		return child;
 	}
 
-	stringifyPopulation() {
-		let formated = '';
-		for(let individual of this.population) {
-			let word = individual.join('');
-			formated += word + '\n';
-		}
-		return formated;
+    /**
+     * I create a single string representation of my population.
+     *
+     * Returns:
+     *  formated:
+     */
+    stringifyPopulation()
+    {
+        // setup string representation
+        let formatted = '';
+
+        // for each individual of population
+        for(let individual of this.population)
+        {
+            // get single string genotype representation
+            let genotype = individual.join('');
+
+            // add it to population representation
+			formatted += genotype + '\n';
+        }
+
+        // return population string representation
+		return formatted;
 	}
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,51 +1,90 @@
+/**
+ * State
+ */
+
+// genetic algorithm parameters
 const POOL = 'ABCDEFGHIJKLMNOPQRSTUVXWYZ ';
 const POPULATION_SIZE = 1500;
 const MUTATION_RATE = 0.005;
 const TARGET = 'PEOPLE DOING STRANGE THINGS WITH SOFTWARE'
 
+// sketch containers
 let ga;
 let fit;
 let counter = 1;
 let img;
 
-function randomColor() {
+/**
+ * I generate a random color.
+ *
+ * Returns:
+ * 	randomColor(Array): generated color
+ */
+function randomColor()
+{
 	return [random(0, 255), random(0, 255), random(0, 255)];
 }
 
-function preload() {
+/**
+ * p5.js preload assets function.
+ */
+function preload()
+{
 	img = loadImage('../assets/darwin.jpg');
-  }
+}
 
-function setup() {
+
+/**
+ * p5.js setup function.
+ */
+function setup()
+{
+	// create drawing canvas
 	createCanvas(windowWidth-200, windowHeight-50);
+
+	// create genetic algorithm abstraction
 	ga = new GeneticAlgorithm(TARGET, POPULATION_SIZE, MUTATION_RATE);
 }
 
-function draw() {
+/**
+ * p5.js loop.
+ */
+function draw()
+{
+	// compute current population fitness
 	fit = ga.computeFitness();
+
+	// target genotype not reached: mate next generation
 	if(fit.bestFitness < TARGET.length) {
 		ga.generation++;
 		ga.mutate();
 		ga.mate(fit.populationFitness)
 	}
 
+	// clear sketch
 	background(25);
+
+	// draw Darwin's image
 	image(img, windowWidth / 4, windowHeight / 1.5, img.width / 2.5, img.height / 2.5);
 
+	// draw population string representation
 	fill(...randomColor());
 	textFont('Gloria Hallelujah', 20);
 	text(ga.stringifyPopulation(), windowWidth*0.6 - 50, 20, windowWidth*0.6 -50, windowHeight - 60);
 
+	// create best genotype string representation
 	let bestPhrase = ga.population[fit.bestFitnesssPosition].join('')
 	bestPhrase = bestPhrase.slice(0, 7) + '\n' +
 				 bestPhrase.slice(7, 13) + '\n' +
 				 bestPhrase.slice(13, 28) + '\n' +
 				 bestPhrase.slice(28, bestPhrase.length);
 
+	// draw it
 	fill(...randomColor());
 	textFont('Indie Flower', 100);
 	text(bestPhrase, 20, windowHeight*0.1, windowWidth, windowHeight);
 
+	// draw current population stats
 	fill(...randomColor());
 	textFont('Indie Flower', 30);
 	text(`Generation: ${ga.generation}`, 20, windowHeight*0.76, windowWidth * 0.3, windowHeight * 0.7)
